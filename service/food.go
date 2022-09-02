e366a1d0046b6feb1e1cf3ebf1e50ed34d4373aa
xueliuyang <xueliuyang@xingshulin.com>
2022-09-02 19:13:02 +0800

    修改菜单接口，返回json对象

--- service/food.go
+++ service/food.go
@@ -1,13 +1,26 @@
 package service
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/hsuehly/presideService/db"
 	"github.com/hsuehly/presideService/models"
 )
 
-func GetFoodByIdService(id string) string {
+type FoodMenu struct {
+	FoodTime     string `json:"foodTime"`
+	FoodName     string `json:"foodName"`
+	FoodImageURl string `json:"foodImageURL"`
+}
+
+func GetFoodByIdService(id string) (foodMenu []FoodMenu) {
 	var result string
 	db.Get().Model(&models.FoodMenu{}).Select("data").First(&result, "id = ?", id)
-	return result
+	err := json.Unmarshal([]byte(result), &foodMenu)
+	if err != nil {
+		fmt.Println("err1", err)
+	}
+
+	return
 
 }
